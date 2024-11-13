9ea35d0aee75f837eccc1bb66a58f4e78358c9c5
Honey644 <honeykumar644@gmail.com>
2024-11-13 21:56:29 +0530

    Feature/crud book service (#1)
    * Crud Book Service Operations
    
    * Crud Book Service Operations updated

--- src/main/java/com/library/BookService/Service/BookService.java
+++ src/main/java/com/library/BookService/Service/BookService.java
@@ -1,4 +1,13 @@
 package com.library.BookService.Service;
 
-public class BookService {
+import com.library.BookService.Dto.BookDto;
+
+import java.util.List;
+
+public interface BookService {
+    BookDto createBook(BookDto book);
+    List<BookDto> getAllBooks();
+    BookDto getBookById(Long id);
+    BookDto updateBook(BookDto book);
+    void deleteBookById(Long id);
 }
